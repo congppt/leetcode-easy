e3a6978d70bb10603d18c379f3dbe54138b7c466
congppt <congppt2002@gmail.com>
2024-07-24 16:18:15 +0700

    problem 35

--- Leetcode/Program.cs
+++ Leetcode/Program.cs
@@ -1,2 +1,2 @@
 ﻿using Leetcode;
-Console.WriteLine(RemoveDuplicatesFromSortedArrayProblem.RemoveDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+Console.WriteLine(SearchInsertPositionProblem.SearchInsert([1,3,5,6], 5));
--- Leetcode/SearchInsertPositionProblem.cs
+++ Leetcode/SearchInsertPositionProblem.cs
@@ -1,12 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Leetcode
 {
     public class SearchInsertPositionProblem
     {
+        public static int SearchInsert(int[] nums, int target)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+            while (left <= right)
+            {
+                var mid = (right + left)/2;
+                if (target == nums[mid]) return mid;
+                if (target > nums[mid])
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            return left;
+        }
     }
 }
