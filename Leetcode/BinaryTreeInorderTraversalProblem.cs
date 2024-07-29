cdc61ec4ab3f97217babd58dc13e1f4a343fce7a
congppt <congppt2002@gmail.com>
2024-07-29 22:35:18 +0700

    problem 94

---
+++ Leetcode/BinaryTreeInorderTraversalProblem.cs
@@ -0,0 +1,44 @@
+namespace Leetcode
+{
+    public class BinaryTreeInorderTraversalProblem
+    {
+        public IList<int> InorderTraversal(TreeNode? root)
+        {
+            if (root == null) return [];
+            List<int> result = [];
+            //InorderVisit(root, ref result);
+            Stack<TreeNode> tree = [];
+            while (root != null || tree.Count > 0)
+            {
+                while (root != null)
+                {
+                    tree.Push(root);
+                    root = root.left;
+                }
+                root = tree.Pop();
+                result.Add(root.val);
+                root = root.right;
+            }
+            return result;
+        }
+        //public void InorderVisit(TreeNode? root, ref List<int> visited)
+        //{
+        //    if (root == null) return;
+        //    if (root.left != null) InorderVisit(root.left, ref visited);
+        //    visited.Add(root.val);
+        //    if (root.right != null) InorderVisit(root.right, ref visited);
+        //}
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode? left;
+            public TreeNode? right;
+            public TreeNode(int val = 0, TreeNode? left = null, TreeNode? right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+        }
+    }
+}
