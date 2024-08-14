753def6964492f2f9d36984b0c757bbe04a545ad
congppt <congppt2002@gmail.com>
2024-08-14 12:05:48 +0700

    problem 349

---
+++ Leetcode/IntersectionOfTwoArraysProblem.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode
+{
+    public class IntersectionOfTwoArraysProblem
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            HashSet<int> result = [];
+            HashSet<int> nums1Values = [];
+            foreach (var num in nums1)
+                nums1Values.Add(num);
+            foreach (var num in nums2)
+                if (nums1Values.Contains(num))
+                    result.Add(num);
+            return [.. result];
+        }
+    }
+}
