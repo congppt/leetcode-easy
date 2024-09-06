de997f54757ec013c14931020a1e484a09c13733
congppt <congppt2002@gmail.com>
2024-09-06 18:51:04 +0700

    problem 197

---
+++ Leetcode/RisingTemperatureProblem.sql
@@ -0,0 +1,2 @@
+with yesterdayTemp as (select id, DATE_ADD(recordDate, interval 1 day) as recordDate , temperature from Weather)
+select d.id from Weather as d join yesterdayTemp as y on d.recordDate = y.recordDate and d.temperature > y.temperature
