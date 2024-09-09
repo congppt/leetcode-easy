05792f37a5ee973822d11428a139ac438f22e35c
congppt <congppt2002@gmail.com>
2024-09-09 08:13:44 +0700

    problem 706

--- Leetcode/DesignHashMapProblem.cs
+++ Leetcode/DesignHashMapProblem.cs
@@ -9,26 +9,70 @@ namespace Leetcode
     {
         public class MyHashMap
         {
-            private int[] _map;
+            private LinkedList<KeyValuePair<int, int>>[] _map;
+            private int _size;
             public MyHashMap()
             {
-                _map = new int[1000001];
+                _map = new LinkedList<KeyValuePair<int, int>>[2];
             }
 
             public void Put(int key, int value)
             {
-                _map[key] = value + 1;
+                if (_size == _map.Length) Resize();
+                var index = key % _map.Length;
+                if (_map[index] == null)
+                {
+                    _map[index] = new LinkedList<KeyValuePair<int, int>>();
+                    _map[index].AddFirst(new KeyValuePair<int, int>(key, value));
+                    _size++;
+                    return;
+                }
+                var iter = _map[index].First;
+                while (iter != null)
+                {
+                    if (iter.Value.Key == key)
+                    {
+                        iter.Value = new KeyValuePair<int, int>(key, value);
+                        return;
+                    }
+                    iter = iter.Next;
+                }
+                _size++;
+                _map[index].AddLast(new KeyValuePair<int, int>(key, value));
+
             }
 
             public int Get(int key)
             {
-                if (_map[key] >= 1) return _map[key] - 1;
+                var index = key % _map.Length;
+                if (_map[index] == null) return -1;
+                foreach (var pair in _map[index])
+                    if (pair.Key == key) return pair.Value;
                 return -1;
             }
 
             public void Remove(int key)
             {
-                _map[key] = 0;
+                var index = key % _map.Length;
+                if (_map[index] == null) return;
+                foreach (var pair in _map[index])
+                    if (pair.Key == key)
+                    {
+                        _map[index].Remove(pair);
+                        _size--;
+                        break;
+                    }
+
+            }
+            private void Resize()
+            {
+                var temp = _map;
+                _map = new LinkedList<KeyValuePair<int, int>>[_size * 2];
+                _size = 0;
+                for (int i = 0; i < temp.Length; i++){
+                    if (temp[i] == null) continue;
+                    foreach (var pair in temp[i]) Put(pair.Key, pair.Value);
+                }
             }
         }
     }
