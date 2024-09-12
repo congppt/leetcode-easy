5e1c910b2e218b291e19670294022b0e16f23de4
congppt <congppt2002@gmail.com>
2024-09-13 06:44:50 +0700

    problem 783

--- Leetcode/MinimumDistanceBetweenBSTNodesProblem.cs
+++ Leetcode/MinimumDistanceBetweenBSTNodesProblem.cs
@@ -7,6 +7,8 @@ namespace Leetcode
 {
     public class MinimumDistanceBetweenBSTNodesProblem
     {
+        private int minimalDiff = int.MaxValue;
+        private int prevValue = -1;
         public int MinDiffInBST(TreeNode? root)
         {
             Stack<TreeNode> stack = [];
@@ -18,11 +20,18 @@ namespace Leetcode
                     root = root.left;
                 }
                 root = stack.Pop();
-                if (prevVal > 0) minDiff = Math.Min(minDiff, root.val - prevVal);
+                if (prevVal >= 0) minDiff = Math.Min(minDiff, root.val - prevVal);
                 prevVal = root.val;
                 root = root.right;
             }
             return minDiff;
+        }
+        void MinDiffInBSTRecursive(TreeNode? root){
+            if (root == null) return;
+            if (root.left != null) MinDiffInBSTRecursive(root.left);
+            if (prevValue >= 0) minimalDiff = Math.Min(minimalDiff, root.val - prevValue);
+            prevValue = root.val;
+            if (root.right != null) MinDiffInBSTRecursive(root.right);
         }
         public class TreeNode
         {
