a0aca6e3e82bfaff2d748886df3ac777d3b9ee31
CongPPT <congppt2002@gmail.com>
2024-10-24 08:50:33 +0700

    problem 796

---
+++ Leetcode/RotateStringProblem.cs
@@ -0,0 +1,9 @@
+﻿namespace Leetcode;
+
+public class RotateStringProblem
+{
+    public bool RotateString(string s, string goal)
+    {
+        return s.Length == goal.Length && (s + s).Contains(goal);
+    }
+}
