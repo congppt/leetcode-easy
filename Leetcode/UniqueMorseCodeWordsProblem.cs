b3a2e30fbbefebceb9fa7691a232477d163993d5
CongPPT <congppt2002@gmail.com>
2024-10-24 09:06:27 +0700

    problem 804

---
+++ Leetcode/UniqueMorseCodeWordsProblem.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace Leetcode;
+
+public class UniqueMorseCodeWordsProblem
+{
+    public int UniqueMorseRepresentations(string[] words)
+    {
+        string[] morse =
+        [
+            ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+            ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."
+        ];
+        HashSet<string> transformations = [];
+        foreach (var word in words)
+        {
+            var builder = new StringBuilder();
+            foreach (var c in word)
+                builder.Append(morse[c - 'a']);
+            transformations.Add(builder.ToString());
+        }
+        return transformations.Count;
+    }
+}
